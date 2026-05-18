96ccbac0ede6aa64ff0138b685042f68cddb3f09
Sultan Abdussalam <abdussultan14@gmail.com>
2026-05-18 23:58:32 +0100

    fix email

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -16,12 +16,12 @@ const getTransporter = () => {
   transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: parseInt(process.env.SMTP_PORT || "587"),
-    secure: false,
+    secure: process.env.SMTP_PORT === "465",
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
-    family: 4
+    family: 4, // Force IPv4 to avoid potential issues in some environments
   });
 
   return transporter;
